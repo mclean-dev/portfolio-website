6a0104a50364547d5c48a54e9043acc5cd17ef80
John McLean <mclean.jb@gmail.com>
2022-04-12 19:19:25 -0400

    Change form submission to Netlify forms

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -2,7 +2,6 @@ import React, {useState } from 'react'
 
 import { images } from '../../constants'
 import { AppWrap, MotionWrap } from '../../wrapper'
-import { client } from '../../client'
 import './Footer.scss'
 
 
@@ -18,22 +17,42 @@ const Footer = () => {
 
     setFormData({...formData, [name]: value})
   }
-  const handleSubmit = () => {
-    setLoading(true);
 
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) =>
+          encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  const handleSubmit = (e) => {
+   
     const contact = {
-      _type: 'contact',
+      "form-name": 'contact',
       name: name,
       email: email,
       message: message
     }
-
-    client.create(contact)
-    .then(() => {
-      setLoading(false)
-      setIsFormSubmitted(true)
+    
+    e.preventDefault();
+    setLoading(true);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode(contact),
     })
+      .then(() => {
+        setTimeout(() => {
+          setLoading(false)
+          setIsFormSubmitted(true)
+          }, 750)
+      })
+      .catch((error) => alert(error));
+   
   }
+
   return (
     <>
     <h2 className="head-text">Have a coffee & chat with me.</h2>
@@ -43,24 +62,26 @@ const Footer = () => {
         <a href="mailto:mclean.jb@gmail.com" className='p-text'>mclean.jb@gmail.com</a>
       </div>
       <div className="app__footer-card">
-        <img src={images.mobile} alt="mobile" />
-        <a href="tel: +1 (919) 880-5025" className='p-text'>+1 (919) 880-5025</a>
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel: +1 (919) 880-5025" className='p-text'>+1 (919) 880-5025</a>
+        </div>
       </div>
-    </div>
-    {!isFormSubmitted ? 
-    <div className="app__footer-form app__flex">
-      <div className="app__flex">
-        <input type="text" className="p-text" placeholder='Your Name' name="name" value={name} onChange={handleChangeInput} />
-      </div>
-      <div className="app__flex">
-        <input type="email" className="p-text" placeholder='Your Email' name="email" value={email} onChange={handleChangeInput} />
-      </div>
-      <div>
+      {!isFormSubmitted ?
+        <form className="app__footer-form app__flex" name='contact' method="POST" onSubmit={handleSubmit}>
+          <input type="hidden" name="form-name" value="contact" />
+
+          <div className="app__flex">
+            <input type="text" className="p-text" placeholder='Your Name' name="name" value={name} onChange={handleChangeInput} />
+          </div>
+          <div className="app__flex">
+            <input type="email" className="p-text" placeholder='Your Email' name="email" value={email} onChange={handleChangeInput} />
+          </div>
+          <div>
         <textarea name="message" placeholder="Your Message" value={message} onChange={handleChangeInput} className="p-text" />
 
       </div>
-      <button className="p-text" type="button" onClick={handleSubmit}>{loading ? 'Sending' : 'Send Messsage' }</button>
-    </div>
+      <button className="p-text" type="submit">{loading ? 'Sending' : 'Send Messsage' }</button>
+    </form>
     : <div><h3 className='head-text'>Thank you for getting in touch!</h3></div>
     }
     </>
