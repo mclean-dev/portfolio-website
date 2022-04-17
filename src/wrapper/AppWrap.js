a67154213b21cab7a883e6d0b93927a4851f1ae4
John McLean <mclean.jb@gmail.com>
2022-04-17 18:47:46 -0400

    Fix: scaling for large screens.

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -7,9 +7,9 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
             <div className='app__wrapper app__flex'>
                 <Component />
 
-                <div className='copyright'>
-                    <p className='p-text'>@2022 JOHN MCLEAN</p>
-                    <p className='p-text'>All rights reserved.</p>
+                <div className='copyright' >
+                    <p className='c-text'>@2022 JOHN MCLEAN</p>
+                    <p className='c-text'>All rights reserved.</p>
                 </div>
             </div>
             <NavigationDots active={idName} />
