120b09b1549c2833752ed04e905cca341fa37ce8
John McLean <mclean.jb@gmail.com>
2022-06-21 21:47:20 -0400

    Remove empty links from SoundDev projects

--- frontend-react/src/container/Portfolio/Portfolio.jsx
+++ frontend-react/src/container/Portfolio/Portfolio.jsx
@@ -1,55 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import { AiFillEye, AiFillGithub } from 'react-icons/ai'
-import { motion } from 'framer-motion'
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
 
-import { AppWrap, MotionWrap } from '../../wrapper'
-import axios from 'axios'
+import { AppWrap, MotionWrap } from "../../wrapper";
+import axios from "axios";
 
-import './Portfolio.scss'
+import "./Portfolio.scss";
 
 const Portfolio = () => {
-  const [activeFilter, setActiveFilter] = useState('All')
-  const [animateCard, setAnimateCard] = useState([{ y: 0, opacity: 1 }])
-  const [portfolio, setPortfolio] = useState([])
-  const [filterPortfolio, setFilterPortfolio] = useState([])
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState([{ y: 0, opacity: 1 }]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [filterPortfolio, setFilterPortfolio] = useState([]);
 
   useEffect(() => {
     const query = '*[_type == "portfolio"] | order(order asc)';
-    axios.get('/.netlify/functions/getter', { params: { "query": `${query}`  } })
-    .then((data) => {      
-      setPortfolio(data.data);
-      setFilterPortfolio(data.data);
-    })
- 
-  
-  }, [])
-  
+    axios
+      .get("/.netlify/functions/getter", { params: { query: `${query}` } })
+      .then((data) => {
+        setPortfolio(data.data);
+        setFilterPortfolio(data.data);
+      });
+  }, []);
+
   const handlePortfolioFilter = (item) => {
     setActiveFilter(item);
-    setAnimateCard([{y: 100, opacity: 0}]);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
 
     setTimeout(() => {
-      setAnimateCard([{y: 0, opacity: 1}]);
+      setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      if (item === 'All') {
+      if (item === "All") {
         setFilterPortfolio(portfolio);
- 
       } else {
-        setFilterPortfolio(portfolio.filter((portfolio) => portfolio.tags.includes(item))); 
+        setFilterPortfolio(
+          portfolio.filter((portfolio) => portfolio.tags.includes(item))
+        );
       }
-    }, 500)
-
-  }
+    }, 500);
+  };
   return (
     <>
-      <h2 className='head-text'>My Creative <span>Portfolio</span></h2>
+      <h2 className="head-text">
+        My Creative <span>Portfolio</span>
+      </h2>
 
       <div className="app__portfolio-filter">
-        {['UI/UX', 'Web App', 'React JS', 'All'].map((item, index) => (
+        {["UI/UX", "Web App", "React JS", "All"].map((item, index) => (
           <div
             key={index}
             onClick={() => handlePortfolioFilter(item)}
-            className={`app__portfolio-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+            className={`app__portfolio-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
           >
             {item}
           </div>
@@ -57,9 +60,8 @@ const Portfolio = () => {
       </div>
       <motion.div
         animate={animateCard}
-        transition={{duration: 0.5, delayChildren: 0.5 }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__portfolio-projects"
-
       >
         {filterPortfolio.map((portfolio, index) => (
           <div className="app__portfolio-item app__flex" key={index}>
@@ -67,39 +69,67 @@ const Portfolio = () => {
               <img src={portfolio.image} alt={portfolio.name} />
               <motion.div
                 whileInView={{ opacity: [1, 0] }}
-                whileHover={{opacity: [0, 1]}}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
-                className="app__portfolio-hover app__flex"
-
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+                className={`app__portfolio-hover app__flex ${
+                  portfolio.tags.includes("SoundDev") ? "SoundDev" : ""
+                }`}
               >
-                <a href={portfolio.projectLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{scale: [0, 1]}}
-                    whileHover={{scale: [1, 0.9]}}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillEye />
-                  </motion.div>
-                </a>
-                <a href={portfolio.codeLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{scale: [0, 1]}}
-                    whileHover={{scale: [1, 0.9]}}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillGithub />
-                  </motion.div>
-                </a>
+                {portfolio.tags.includes("SoundDev") ? (
+                  <></>
+                ) : (
+                  <>
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex portfolio-link"
+                    >
+                      <a
+                        href={portfolio.projectLink}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="app__flex"
+                      >
+                        <AiFillEye />{" "}
+                      </a>
+                    </motion.div>
+
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex portfolio-link"
+                    >
+                      <a
+                        href={portfolio.codeLink}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="app__flex"
+                      >
+                        <AiFillGithub />{" "}
+                      </a>
+                    </motion.div>
+                  </>
+                )}
               </motion.div>
             </div>
 
             <div className="app__portfolio-content app__flex">
               <h4 className="bold-text">{portfolio.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>{portfolio.description}</p>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {portfolio.description}
+              </p>
               <div className="app__portfolio-content-links">
-              <a href={portfolio.portfolioLink} target="_blank" rel="noreferrer">
+                <a
+                  href={portfolio.portfolioLink}
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <div className="app__flex">
                     <AiFillEye />
                   </div>
@@ -118,7 +148,11 @@ const Portfolio = () => {
         ))}
       </motion.div>
     </>
-  )
-}
+  );
+};
 
-export default AppWrap(MotionWrap(Portfolio, 'app__portfolio'),'portfolio','app__whitebg')
+export default AppWrap(
+  MotionWrap(Portfolio, "app__portfolio"),
+  "portfolio",
+  "app__whitebg"
+);
